81bd8dbdd4bed05d4034c6249d4b41f8584546e7
teachforaustralia <itmanager@teachforaustralia.org>
2025-07-10 01:01:42 +1000

    Add files via upload

---
+++ taskpane.js
@@ -0,0 +1,13 @@
+function addBccAddress(event) {
+  Office.context.mailbox.item.bcc.addAsync(
+    { emailAddress: "emailtosalesforce@2lb252qj41tlq82ooamrh5mvk5uidil4qtbu88surrqi7brj1y.8-a0kdeay.na104.le.salesforce.com" },
+    function (asyncResult) {
+      if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+        console.log("BCC added successfully");
+      } else {
+        console.error("Failed to add BCC", asyncResult.error);
+      }
+      event.completed();
+    }
+  );
+}
