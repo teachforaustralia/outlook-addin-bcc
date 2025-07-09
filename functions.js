d3570526ee914fc635e6e7221f367d1b678b033a
teachforaustralia <itmanager@teachforaustralia.org>
2025-07-10 01:33:45 +1000

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,13 @@
+function addBccAddress(event) {
+    Office.context.mailbox.item.bcc.addAsync(
+        { emailAddress: "itmanager@teachforaustralia.org" },
+        function (asyncResult) {
+            if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+                console.error(asyncResult.error.message);
+            } else {
+                console.log("BCC address added successfully.");
+            }
+            event.completed();
+        }
+    );
+}
